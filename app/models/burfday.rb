57623097aa71916dac39d584a0222fd0ab360bc0
dwightgnjohnson <dwightgnjohnson@gmail.com>
2014-12-02 21:57:31 -0600

    styled better, DOB method gives age

--- app/models/burfday.rb
+++ app/models/burfday.rb
@@ -5,4 +5,10 @@ class Burfday < ActiveRecord::Base
   mount_uploader :photo, PhotoUploader
 
   belongs_to :user
+
+  def dob
+    now = Time.now.utc.to_date
+    now.year - age.year - ((now.month > age.month || (now.month == age.month && now.day >= age.day)) ? 0 : 1)
+  end
+
 end
